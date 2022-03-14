7c052c70f5474e4a30f4b71104c7eaf6429b0ede
Gustavo Augusto <gstvoaugusto@gmail.com>
2022-03-14 14:49:15 -0300

    Comenting

--- JS/Calculator (Calculadora)/back-end.js
+++ JS/Calculator (Calculadora)/back-end.js
@@ -1,15 +1,21 @@
 function getDados(){
+  //Getting the values 
   var firstNumber = document.querySelector('.firstNumber').value;
   var secondNumber = document.querySelector('.secondNumber').value;
+  //Choosing the operation
   var operator = document.querySelector('#operator').value;
+  //Sending to receptDados 
   var results = this.receptDados(firstNumber, secondNumber, operator);
+  //Calculating the answer
   document.querySelector('#results').innerHTML = results;
 }
 
 function receptDados(firstNumber, secondNumber, operator){
+  //String to Number
   var firstNumber = Number(firstNumber);
   var secondNumber = Number(secondNumber);
 
+  //Making the counts
   if(operator == '+'){
     var results = firstNumber + secondNumber;
   }
@@ -25,5 +31,6 @@ function receptDados(firstNumber, secondNumber, operator){
   if(operator == '/'){
     var results = firstNumber / secondNumber;
   }
+  //Information returns
   return results;
 }
